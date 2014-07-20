f829b2b4a56a1097f2d8e90ec7750791237d873c
Weathers2015 <soccerplayer135@msn.com>
2014-07-20 18:03:14 -0500

    Added a section for my code.
    Have more information for you.  Things might be easier than we expected.
    Key word is might. haha

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <string>
 #include <sstream>
 using namespace std;
 
-
-void brandon() {
+/*void brandon() {
     while (true) {
         
         int maxDigits = 0;
@@ -59,4 +60,127 @@ int main(int argc, const char * argv[])
     brandon();
     return 0;
 }
+*/
 
+//-------------------------------------------------------------//
+//
+//  Created by Brandon Weathers on 7/20/14.
+//  Copyright (c) 2014 Brandon Weathers. All Rights Reserved.
+//
+
+void sort();
+void compare();
+void cellCheck(int digits[], int length);
+void convertToNumber();
+void convertToArray();
+
+int main()
+{
+	cout << "What digit length would you like to test?" << endl;
+	cout << "Length: ";
+	int length;
+	cin >> length;
+
+	//Creates the initial array for testing//
+	int *digits;
+	digits = new int[length];
+
+	//Place Holder Array//
+	int *prev;
+	prev = new int[length];
+
+	//Place Holder Array//
+	int *cur;
+	cur = new int[length];
+
+
+
+	for (int i = 0; i < length; i++)
+		digits[i] = 0;
+
+	cellCheck(digits, length);
+
+	do
+	{
+		
+	} while (cur != prev);
+
+	
+
+	return 0;
+}
+
+
+
+void sort()
+{
+
+
+}
+
+
+
+void compare()
+{
+
+
+}
+
+
+
+void cellCheck(int digits[], int length)
+{
+	int flag = 0;
+	
+	cout << endl << digits[0] << digits[1] << digits[2] << endl;;
+	
+	for (int i = 0; i < length - 1; i++)
+	{
+		if (digits[i] == digits[i + 1])
+		{
+			flag = flag + 1;
+		}
+	}
+
+	if (flag == length-1)
+	{
+		cout << endl;
+		cout << "All values of the array are the same!" << endl;
+		cout << "The array contains " << length << " " << digits[0] << "\'s." << endl << endl;
+	}
+	else
+	{
+		cout << endl;
+		cout << "Not all values in this array are the same.  ";
+		
+		cout << "The values in the array are: ";
+		for (int i = 0; i < length; i++)
+			cout << digits[i] << " ";
+		
+		cout << endl << endl;
+	}
+		
+}
+
+
+
+void convertToNumber()
+{
+	//Something like this//
+	/*
+	int temp;
+	stringstream convert(number);
+	if (!(convert >> temp))
+		temp = 0;
+
+	cout << endl << endl;
+	cout << temp + 1;
+	*/
+}
+
+
+
+void convertToArray()
+{
+
+}
